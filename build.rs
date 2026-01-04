67865a9dfefa012e6ac1b34cb22c2acf4d0cea98
Vlad M <webmonch@gmail.com>
2026-01-04 15:40:49 +0000

    Add macos linker flag

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("cargo:rustc-env=FFMPEG_DIR=/Users/jonathanburger/rust-ffmpeg-splitter/remotion");
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-headerpad_max_install_names");
 }
